2143b85ec81b1ef7e485ce570730561c63a2a40d
jina <pja9362@g.skku.edu>
2023-07-10 16:45:30 +0900

    feat(회원가입,로그인): 백엔드 로직 수정

--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -1,15 +1,12 @@
-// routes/auth/index.js
 const express = require('express');
 const auth = express.Router();
-const signupRouter = require('./signup');
-const loginRouter = require('./login');
+const authRouter = require('./auth');
 
 auth.use((req, res, next) => {
   console.log("API for auth");
   next();
-})
+});
 
-auth.use('/signup', signupRouter);
-auth.use('/login', loginRouter);
+auth.use('/', authRouter);
 
 module.exports = auth;
