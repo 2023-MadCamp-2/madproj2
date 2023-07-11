38f280949a4d8a648bb6d349ea81370e08dc56b3
jina <pja9362@gmail.com>
2023-07-11 13:36:27 +0000

    feat(Auth, contact): 중복확인 등 수정

--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const auth = express.Router();
 const signupRouter = require('./signup');
 const loginRouter = require('./login');
+const checkNickname = require('./checkNickname');
+const findUsername = require('./user');
 
 auth.use((req, res, next) => {
   console.log("API for auth");
@@ -10,18 +12,7 @@ auth.use((req, res, next) => {
 
 auth.use('/signup', signupRouter);
 auth.use('/login', loginRouter);
+auth.use('/checkNickname', checkNickname);
+auth.use('/findUsername', findUsername);
 
 module.exports = auth;
-
-// const express = require('express');
-// const auth = express.Router();
-// const authRouter = require('./auth');
-
-// auth.use((req, res, next) => {
-//   console.log("API for auth");
-//   next();
-// });
-
-// auth.use('/', authRouter);
-
-// module.exports = auth;
