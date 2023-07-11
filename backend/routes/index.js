0be5f389c5d476bb8b84f9ccd756f391960b0589
jina <pja9362@gmail.com>
2023-07-11 13:58:55 +0000

    라우팅 index.js 에 chat 추가

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 const auth = require("./auth");
 const contact = require("./contact/contact");
+const chat = require("./chat/chat");
 
 router.get("/", (req, res) => {
     res.locals.title = "Node Chat!";
@@ -12,5 +13,6 @@ router.get("/", (req, res) => {
 
 router.use("/auth", auth);
 router.use("/contact", contact);
+router.use("/chat", chat);
 
 module.exports = router;
